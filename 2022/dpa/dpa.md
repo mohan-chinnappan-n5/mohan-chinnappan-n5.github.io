6f75601a07c78f4bce4041e3b08ca25b440691d6
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2022-02-06 19:36:09 -0500

    forms added

--- 2022/dpa/dpa.md
+++ 2022/dpa/dpa.md
@@ -1,3 +1,19 @@
+# Salesforce Automation Suite
+- Fully Declarative (no/low code)
+    - Workflow Rules
+    - Process Builder
+    - Flow
+- Digital Process Automation (DPA) (no code)
+    - OmniScript
+    - FlexCards
+    - Data Tools
+    - Rule Engine
+    - Document Generation
+- Full Programming (code)
+    - Apex
+    - LWC
+    - AppExchange
+
 # About Digital Process Automation (DPA)
 - Automates Processes which requires human interactions
 - Improves the User Experience with the help of automation
@@ -31,8 +47,32 @@ deliver digital-first and industry-specific experiences across multiple devices 
 ## Architecture
 ![OS arch](img/os-arch-1.png)
 
+- Experience
+    - FlexCards
+        - UI Component with excellent Customer Experience
+    - OmniScript
+        - Design Multi-person omni-channel processes
+
+    - Bots
+        - AI powered interactions
+
+
+- Workflow
+    - Decision Tables
+        - Rules to support complex  process
+    - Data Processing Engine
+        - Process Rules and Perform calculations within a process
+
+    - Intelligent Document Automation (IDA)
+        - Generate Documents and automate workflow
+
+- Integration
+    - Data Raptor
+        - Native no-code data **transformation**
+    - Integration Procures (IPs)
+        - Ingest data from multiple data sources
 ### Digital Experience Layer
-- FlexCards
+- FlexCards (display data and launch actions)
     - components of these 360° views
     - the Beginning and Ending Points for Customer Transactions
     - ![Flexcard-1](img/os/flexCard-1.png)
@@ -49,6 +89,7 @@ deliver digital-first and industry-specific experiences across multiple devices 
 - The console itself is a Salesforce Lightning console
 
 - OmniScripts (/lightning/n/vlocity_ins__OmniScriptHome)
+    - contain the interaction logic
     - Provides a guided path to complete a business process
     - Serves as a **configurable way** of creating a seamless customer experience  
 
@@ -63,6 +104,7 @@ deliver digital-first and industry-specific experiences across multiple devices 
 - OmniStudio Integration Procedures (IP)
     - Provides declarative, server-side processes that execute multiple actions in **a single server call**
 
+- ![dataraptor-1](img/os/dataraptor-1.png)
 - DataRaptors:  a mapping tool provides
     - Configurable services for 
         - retrieving 
@@ -71,7 +113,26 @@ deliver digital-first and industry-specific experiences across multiple devices 
         - transforming (Transform)
         - updating data (Load) 
             - Manipulate any data that comes from inside or outside Salesforce
+    - Can read/write sObjects
+    - xml2json 
+    - Access any REST service
+- API Calls
+- ![api calls](img/os/os-api-calls-1.png)
 
+- FlexCard Actions
+    - have link to
+        - FlexCard 
+        - Pages
+        - URLs
+        - OmniScripts
+        - OmniStudio Actions
+    - Launch in context (e.g. %AccountId%)from one screen
+
+- ![Action items](img/os/action-items-1.png)
+
+- States 
+    - Single FlexCard is rendered differently based a value of a certain field
+- ![FlexCard States](img/os/flexCard-states-1.png)
 
 # Using OmniStudio
 
@@ -111,22 +172,44 @@ deliver digital-first and industry-specific experiences across multiple devices 
 ## Querying   
 - ![Querying ](img/os/query-omscript-1.png)
 
+
+# Remote Site URLS
+- [Docs](https://docs-vlocity-help-center.s3.amazonaws.com/)
+- [IPcommmunity](https://omnistudiotraining-developer-edition.na162.force.com/s/)
+
+
+
 # Resources
 - [digital process automation - TechTarget](https://www.techtarget.com/searchcio/definition/digital-process-automation)
 
 - [7 Benefits with DPA](https://www.cetrixcloudservices.com/blog/7-benefits-realized-through-digital-process-automation)
 
 ## OmniStudio
+
+- [Docs - Vlocity Help Center](https://docs.vlocity.com/en/FlexCard-Designer-Overview.html)
+- [OmniStudio Foundation](https://developer.salesforce.com/files/credential-resources/OmniStudio-Documentation.pdf)
+### Trailhead
 - [Learn OmniStudio on Trailhead](https://trailhead.salesforce.com/en/promo/orgs/omnistudiotrails)
 
-- [Using Digital Process Automation To Improve Customer & Employee Experiences | Salesforce](https://www.youtube.com/watch?v=B-7Z-wMRm0A)
+- [OmniStudio Architecture](https://trailhead.salesforce.com/content/learn/modules/omnistudio-architecture?trail_id=build-guided-experiences-with-omnistudio)
 
-
+### Data sheets
 - [Build digital-first experiences tailored to your industry](https://www.salesforce.com/products/omnistudio/overview/)
 
 - [OmniStudio Datasheet](https://www.salesforce.com/content/dam/web/en_us/www/documents/industries/omnistudio-datasheet.pdf)
 
-- [OmniStudio Architecture](https://trailhead.salesforce.com/content/learn/modules/omnistudio-architecture?trail_id=build-guided-experiences-with-omnistudio)
+
+
+
+### Videos
+
+
+- [Using Digital Process Automation To Improve Customer & Employee Experiences | Salesforce](https://www.youtube.com/watch?v=B-7Z-wMRm0A)
+
+- [Omnistudio Development Tools, Tips and Tricks of passing Data | OmniStudio Best Practices
+](https://www.youtube.com/watch?v=2ftoYvazRlg)
+
+
 # Creation 
 ```
 sfdx mohanc:slides:gen -i dpa.md -o dpa.md.html -t 'Digital Process Automation'
---
+++ 2022/forms/forms/personalAccountOpen.js
@@ -0,0 +1,199 @@
+const programInfo = {
+    "title": "Personal Account Opening ",
+    "id": "PersonAccountOpeningForm",
+    "sfdcAccessToken": "00D8c000003TgUT!AR4AQLbZGtIxlCEiDHqC2.xs.zxkFfm0MbhOb7JRbK0tXn5EwYU2FYOPTxMkQv3WZczVOT.0irCx.9WM1pYwyXTD83A54Zmt",
+    "fields": [
+        {
+            "id": "FirstName",
+            "label": "FirstName",
+            "type": "field",
+            "required": true,
+            "help": {
+                "msg": "Enter your First Name"
+            },
+            "field": {
+                "type": "text",
+                "default": ""
+                
+            }
+        },
+
+        {
+            "id": "LastName",
+            "label": "LastName",
+            "type": "field",
+            "required": true,
+            "help": {
+                "msg": "Enter your Last Name"
+            },
+            "field": {
+                "type": "text",
+                "default": ""
+                
+            }
+        },
+
+         
+
+        {
+            "id": "CurrentTime",
+            "label": "Current Time From Server",
+            "type": "field",
+            "required": true,
+            "help": {
+                "msg": "Current Time will be obtained from the server"
+            },
+            "field": {
+                "type": "REST",
+                "jsonPath": "$.time",
+                "default": "",
+                "url":  "https://mohansun-rum.herokuapp.com/time"
+            }
+        },
+
+
+        {
+            "id": "APIStatus",
+            "label": "Weather API Status",
+            "type": "field",
+            "required": true,
+            "help": {
+                "msg": "Weather API Status"
+            },
+            "field": {
+                "type": "REST",
+                "jsonPath": "$.status",
+                "default": "",
+                "url":  "https://api.weather.gov/"
+            }
+        },
+        {
+            "id": "CatFact",
+            "label": "Cat Facts",
+            "type": "field",
+            "required": true,
+            "help": {
+                "msg": "Cat Fact from API"
+            },
+            "field": {
+                "type": "REST",
+                "jsonPath": "$.fact",
+                "default": "",
+                "url":  "https://catfact.ninja/fact"
+            }
+        },
+
+        {
+            "id": "OpportunityCount",
+            "label": "Opportunity Count",
+            "type": "field",
+            "required": true,
+            "help": {
+                "msg": "Get Opportunity Count"
+            },
+            "field": {
+                "type": "SOQL",
+                "soql": "SELECT COUNT(Id) OpptyCount FROM Opportunity",
+                "jsonPath": "$[0].OpptyCount",
+                "default": "",
+                "url":  "https://d8c000003tgutea0-dev-ed.my.salesforce.com/services/data/v51.0"
+            }
+        },
+
+
+
+        {
+            "id": "DOB",
+            "label": "Date of Birth",
+            "type": "field",
+            "required": true,
+            "help": {
+                "msg": "Date of Birth "
+            },
+            "field": {
+                "type": "date",
+                "default": "" 
+            }
+        },
+        {
+            "id": "USCitizen",
+            "label": "US Citizen? ",
+            "type": "field",
+            "required": true,
+            "help": {
+                "msg": "Are you a US Citizen?"
+            },
+            "field": {
+                "type": "select",
+                "default": "",
+                "options": [
+                    "Yes",
+                    "No"
+                ]
+            }
+        },
+
+        {
+            "id": "DualCitizen",
+            "label": "Have Dual Citizenship? ",
+            "type": "field",
+            "required": true,
+            "help": {
+                "msg": "Have dual Citizenship?"
+            },
+            "field": {
+                "type": "select",
+                "default": "",
+                "options": [
+                    "Yes",
+                    "No"
+                ]
+            }
+        },
+        {
+            "id": "Employment",
+            "label": "Employment Status",
+            "type": "field",
+            "required": true,
+            "help": {
+                "msg": "Employment Status?"
+            },
+            "field": {
+                "type": "select",
+                "default": "",
+                "options": [
+                    "Employed",
+                    "Retired",
+                    "Self-Employed",
+                    "Student",
+                    "Not Employed"
+                ]
+            }
+        },
+
+        {
+            "id": "SourceOfIncome",
+            "label": "Source of Income?",
+            "type": "field",
+            "required": true,
+            "help": {
+                "msg": "Your source of income"
+            },
+            "field": {
+                "type": "select",
+                "default": "",
+                "options": [
+                    "Employment Income",
+                    "Inheritance or Trust",
+                    "Investment Income",
+                    "Retirement Income",
+                    "Social Security",
+                    "Unemployment",
+                    "Household Income /Homemaker"
+                ]
+            }
+        },
+
+         
+    ]
+}
