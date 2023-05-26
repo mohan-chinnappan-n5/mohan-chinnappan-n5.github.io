89e8a748651a5ad3c7ce8fabb36dd5b4191effd2
mohan chinnappan <mohan.chinnappan.n@gmail.com>
2023-05-26 07:53:43 -0400

    sfurls added

---
+++ utils/js/sfurls.js
@@ -0,0 +1,20 @@
+
+// sfurls.js
+
+const getEle = id => document.getElementById(id);
+
+Split(["#menu", "#content"], { sizes: [40, 60] });
+
+getEle('gen').addEventListener('click', event => {
+    const instanceURL = getEle('instanceURL').value;
+    getEle('OM').setAttribute('href', `${instanceURL}/lightning/setup/ObjectManager/home`);
+    getEle('flows').setAttribute('href', `${instanceURL}/lightning/setup/Flows/home`);
+    getEle('deployStatus').setAttribute('href', `${instanceURL}/lightning/setup/DeployStatus/home`);
+    getEle('installedPackages').setAttribute('href', `${instanceURL}/lightning/setup/ImportedPackage/home`);
+    getEle('apex').setAttribute('href', `${instanceURL}/lightning/setup/ApexClasses/home`);
+    getEle('triggers').setAttribute('href', `${instanceURL}/lightning/setup/ApexTriggers/home`);
+    getEle('connectedApps').setAttribute('href', `${instanceURL}/lightning/setup/ConnectedApplication/home`);
+    getEle('connectBulkDataLoads').setAttribute('href', `${instanceURL}/lightning/setup/AsyncApiJobStatus/home`);
+    getEle('appBuilder').setAttribute('href', `${instanceURL}/lightning/setup/FlexiPageList/home`);
+});
+
