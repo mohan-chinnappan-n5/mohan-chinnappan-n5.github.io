83f7d56e6189f0fb03ac62730540073bd676af40
mohan chinnappan <mohan.chinnappan.n@gmail.com>
2023-05-26 07:56:54 -0400

    sfurls added

--- utils/js/sfurls.js
+++ utils/js/sfurls.js
@@ -7,6 +7,7 @@ Split(["#menu", "#content"], { sizes: [40, 60] });
 
 getEle('gen').addEventListener('click', event => {
     const instanceURL = getEle('instanceURL').value;
+    getEle('setup').setAttribute('href', `${instanceURL}/lightning/setup/SetupOneHome/home`);
     getEle('OM').setAttribute('href', `${instanceURL}/lightning/setup/ObjectManager/home`);
     getEle('flows').setAttribute('href', `${instanceURL}/lightning/setup/Flows/home`);
     getEle('deployStatus').setAttribute('href', `${instanceURL}/lightning/setup/DeployStatus/home`);
