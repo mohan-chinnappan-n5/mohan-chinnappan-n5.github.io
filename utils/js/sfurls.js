a9adc234b5df2d9887af5f3c51f098439ee2f76c
mohan chinnappan <mohan.chinnappan.n@gmail.com>
2023-05-26 14:28:16 -0400

    cleanup

--- utils/js/sfurls.js
+++ utils/js/sfurls.js
@@ -5,21 +5,64 @@ const getEle = id => document.getElementById(id);
 
 Split(["#menu", "#content"], { sizes: [40, 60] });
 
-getEle('gen').addEventListener('click', event => {
-    const instanceURL = getEle('instanceURL').value;
-    getEle('setup').setAttribute('href', `${instanceURL}/lightning/setup/SetupOneHome/home`);
-    getEle('OM').setAttribute('href', `${instanceURL}/lightning/setup/ObjectManager/home`);
-    getEle('flows').setAttribute('href', `${instanceURL}/lightning/setup/Flows/home`);
-    getEle('deployStatus').setAttribute('href', `${instanceURL}/lightning/setup/DeployStatus/home`);
-    getEle('installedPackages').setAttribute('href', `${instanceURL}/lightning/setup/ImportedPackage/home`);
-    getEle('apex').setAttribute('href', `${instanceURL}/lightning/setup/ApexClasses/home`);
-    getEle('triggers').setAttribute('href', `${instanceURL}/lightning/setup/ApexTriggers/home`);
-    getEle('connectedApps').setAttribute('href', `${instanceURL}/lightning/setup/ConnectedApplication/home`);
-    getEle('connectBulkDataLoads').setAttribute('href', `${instanceURL}/lightning/setup/AsyncApiJobStatus/home`);
-    getEle('appBuilder').setAttribute('href', `${instanceURL}/lightning/setup/FlexiPageList/home`);
+ 
 
-    getEle('customLabels').setAttribute('href', `${instanceURL}/lightning/setup/ExternalStrings/home`);
-    getEle('CustomMetadata').setAttribute('href', `${instanceURL}/lightning/setup/CustomMetadata/home`);
+const urls = [
+
+    {"Setup":  'SetupOneHome'  },
+    {"Object Manager":  'ObjectManager' },
+    {"Apex":  'Apex'  },
+    {"ApexTriggers": 'ApexTriggers'},
+    {"Flows": 'Flows'},
+    {"DeployStatus": 'DeployStatus'},
+    {"ObjectManager": 'ObjectManager'},
+    {"InstalledPackages": 'ImportedPackage'},
+
+    {"ConnectedApps": 'ConnectedApplication'},
+
+   {"BulkDataLoads": 'AsyncApiJobStatus'},
+   {"AppBuilder": 'FlexiPageList'},
+
+   {"CustomLabels": 'ExternalStrings'},
+   {"AppBuilder": 'FlexiPageList'},
+
+    {"MatchingRules": 'MatchingRules'},
+    {"CustomMetadata": 'CustomMetadata'},
+    {"EinsteinLeadScoring": 'SalesInsights' },
+    {"Dashboard": 'o/Dashboard'},
+    {"EmailToCase": 'EmailToCase'},
+    {"OrgWideEmailAddresses": "OrgWideEmailAddresses"}
+
+
+
+
+
+];
+
+
+const lgEle = getEle('lg');
+const instanceURL =  getEle('instanceURL').value;
+
+
+for (const url of urls) {
+    const liEle = document.createElement('li');
+    liEle.setAttribute('class', 'list-group-item');
+    const anchorEle = document.createElement('a');
+    anchorEle.setAttribute('href', `${instanceURL}/lightning/setup/${url[Object.keys(url)[0]]}/home`);
+    anchorEle.setAttribute('target', 'new');
+    anchorEle.setAttribute('id',Object.keys(url)[0] );
+    anchorEle.textContent = Object.keys(url)[0];
+    liEle.appendChild(anchorEle);
+    lgEle.appendChild(liEle);
+
+}
+getEle('gen').addEventListener('click', event => {
+    for(const url of urls) {
+        const instanceURL =  getEle('instanceURL').value;
+        if (!instanceURL) instanceURL = "https://d4x000007rxogeaq-dev-ed.my.salesforce.com";
+        getEle(Object.keys(url)[0]).setAttribute('href', `${instanceURL}/lightning/setup/${url[Object.keys(url)[0]]}/home`);
+    }
+
 
 });
 
