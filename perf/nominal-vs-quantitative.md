7470cb35b359b22e404907b499c7690e210045ef
mohan chinnappan <mohan.chinnappan.n@gmail.com>
2023-10-07 01:18:29 -0400

    dt update

---
+++ perf/nominal-vs-quantitative.md
@@ -0,0 +1,17 @@
+Nominal and quantitative are two different types of data that are commonly used in statistics and data analysis. They represent distinct levels of measurement and provide different types of information about the characteristics being studied. Here's an explanation of each:
+
+1. **Nominal Data (Categorical Data):**
+   - Nominal data, also known as categorical data, consists of categories, labels, or distinct groups with no inherent order or ranking.
+   - These categories represent qualitative attributes that can be assigned to items, but there is no meaningful numerical value associated with them.
+   - Examples of nominal data include gender (e.g., male or female), colors (e.g., red, green, blue), and types of animals (e.g., dog, cat, bird).
+   - Nominal data is typically analyzed using frequencies and proportions. You can count how many items fall into each category.
+
+2. **Quantitative Data (Numerical Data):**
+   - Quantitative data, also known as numerical data, consists of numerical values that represent quantities or measurements.
+   - These values can be ordered and subjected to mathematical operations such as addition, subtraction, multiplication, and division.
+   - Quantitative data can be further divided into two subtypes:
+     - **Discrete Data:** Discrete data consists of distinct, separate values with gaps between them. Examples include the number of people in a household or the number of cars in a parking lot.
+     - **Continuous Data:** Continuous data consists of an unbroken range of values and can have fractional or decimal values. Examples include height, weight, and temperature.
+   - Quantitative data allows for various statistical analyses, including measures of central tendency (mean, median, mode), dispersion (variance, standard deviation), and correlation.
+
+In summary, nominal data represents categories or labels, while quantitative data represents numerical values that can be measured and subjected to mathematical operations. Understanding the type of data you are working with is essential when selecting appropriate statistical techniques and visualizations for your analysis.
---
+++ viz/datatable/js/dtapp.js
@@ -0,0 +1,213 @@
+
+// dynamic datatable 
+// mohan chinnappan
+
+const getEle = id => document.getElementById(id);
+
+// Get references to the select elements
+const xAxisSelect = getEle("x-axis-select");
+const yAxisSelect = getEle("y-axis-select");
+
+// Check if a value is already stored in local storage
+const storedXValue = localStorage.getItem("XValue");
+const storedYValue = localStorage.getItem("YValue");
+console.log(storedXValue, storedYValue);
+
+
+
+// Add a change event listener to the dropdown
+xAxisSelect.addEventListener("change", function () {
+    // Store the selected value in local storage
+    localStorage.setItem("XValue", this.value);
+});
+yAxisSelect.addEventListener("change", function () {
+    // Store the selected value in local storage
+    localStorage.setItem("YValue", this.value);
+});
+
+const drawBtn = getEle('draw');
+
+let width = 400;
+let height = 400;
+
+
+
+const queryEle = getEle('query');
+const runEle = getEle('run');
+const thsEle = getEle('header');
+let input = `[{"name": "peach", "qty": 200, "quality": "good"}, {"name":"mango", "qty":700, "quality": "good"} ]`;
+queryEle.value = input;
+
+let params = (new URL(document.location)).searchParams;
+
+//------------ can be called with this parameter d 
+if (params.get('d') !== null) { // got it via query param d
+    input = atob(params.get('d'));
+    queryEle.value = input;
+}
+
+//------- via clipboard
+let c = params.get('c');
+if (c !== null) await navigator.clipboard.readText().then((clipText) => {
+    input = clipText;
+    queryEle.value = input;
+});
+
+if (params.get('w') !== null) {
+    width = params.get('w');
+}
+if (params.get('h') !== null) {
+    height = params.get('h');
+}
+
+
+if (c === 'json') {
+    queryEle.value = input;
+}
+if (c === 'csv') {
+    queryEle.value = input;
+    input = JSON.stringify(d3.csvParse(input));
+} else if (c === 'tsv') {
+    queryEle.value = input;
+    input = JSON.stringify(d3.tsvParse(input));
+}
+
+
+const prepareData = (input) => {
+    const data = JSON.parse(input);
+    let columns = Object.keys(data[0]);
+    let fieldsData = [];
+    let ths = '';
+    thsEle.innerHTML = '';
+    for (const col of columns) {
+        //console.log(col);
+        fieldsData.push({ data: col });
+        ths += `<th>${col}</th>`;
+    }
+    thsEle.innerHTML = ths;
+    return { data, fieldsData };
+}
+
+
+const renderDataTable = (input) => {
+    let data, fieldsData;
+    ({ data, fieldsData } = prepareData(input));
+    // alert(JSON.stringify(fieldsData));
+    // console.log(data, fieldsData);
+    const dt = $('#dt').DataTable({
+        "destroy": true,
+        "bDestroy": true,
+        data: data,
+        columns: fieldsData,
+        dom: 'Blfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    })
+    for (let i = 0; i < dt.columns().nodes().length; i++) {
+        dt.columns(i).header().to$().text(fieldsData[i].data)
+    }
+
+
+    dt.columns.adjust().draw();
+
+    const fieldTypes = determineFieldType(data);
+    setupDropdowns(data);
+
+    drawBtn.addEventListener('click', event => {
+        drawChart(data, fieldTypes, xAxisSelect.value, yAxisSelect.value);
+        getEle('vizBar').focus();
+
+    });
+
+    drawChart(data, fieldTypes, xAxisSelect.value, yAxisSelect.value);
+}
+
+//------------
+
+function determineFieldType(data) {
+    if (!Array.isArray(data) || data.length === 0) {
+        return "Invalid input data";
+    }
+
+    const firstObject = data[0];
+
+    if (typeof firstObject !== "object") {
+        return "Invalid input data";
+    }
+
+    const fieldTypes = {};
+
+    for (const key in firstObject) {
+        if (firstObject.hasOwnProperty(key)) {
+            const value = firstObject[key];
+
+            if (typeof value === "string") {
+                // Check if the field is nominal (contains non-numeric values)
+                fieldTypes[key] = data.every((item) => typeof item[key] === "string")
+                    ? "nominal"
+                    : "quantitative";
+            } else if (typeof value === "number") {
+                // Check if the field is quantitative (contains numeric values)
+                fieldTypes[key] = data.every((item) => typeof item[key] === "number")
+                    ? "quantitative"
+                    : "nominal";
+            } else {
+                // If the field is neither string nor number, it's considered invalid
+                fieldTypes[key] = "Invalid";
+            }
+        }
+    }
+
+    return fieldTypes;
+}
+
+function setupDropdowns(jsonData) {
+
+    // Populate the dropdowns with field names
+    for (const key in jsonData[0]) {
+        if (jsonData[0].hasOwnProperty(key)) {
+            const optionX = document.createElement("option");
+            optionX.value = key;
+            optionX.textContent = key;
+            xAxisSelect.appendChild(optionX);
+
+            const optionY = document.createElement("option");
+            optionY.value = key;
+            optionY.textContent = key;
+            yAxisSelect.appendChild(optionY);
+        }
+    }
+    // Set the initial selection if a value is stored
+    if (storedXValue) {
+        xAxisSelect.value = storedXValue;
+    }
+    if (storedYValue) {
+        yAxisSelect.value = storedYValue;
+    }
+}
+
+// charts
+const drawChart = (data, fieldTypes, x, y) => {
+    const barSpec =
+    {
+        "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+        "description": "Chart",
+        "data": {
+            "values": data
+        },
+
+        "width": width,
+        "height": height,
+
+        "mark": { type: "bar", "tooltip": true, orient: "horizontal" },
+        "encoding": {
+            "x": { "field": x, "type": fieldTypes[x], "axis": { "labelAngle": 0, "grid": true } },
+            "y": { "field": y, "type": fieldTypes[y], "axis": { "labelAngle": 0, "grid": true } },
+        }
+    }
+    //console.log(barSpec);
+    vegaEmbed('#vizBar', barSpec);
+}
+
+renderDataTable(input);
