88ab57739621b2e97053813adbf676c9b633bcfc
mohan chinnappan <mohan.chinnappan.n@gmail.com>
2024-09-19 04:18:11 -0400

    chatty updates

--- apps/3/sections.js
+++ apps/3/sections.js
@@ -11,7 +11,7 @@
         { text: 'Notes on Metadata Deploy Status Monitor', href: '../../deployments/deploy-status.html', icon: 'book' },
         { text: 'Notes on Page Performance Chrome Extension', href: '../../pageperf/pageperf.html', icon: 'book' },
         { text: 'Notes on SF Utils Chrome Extension', href: '../../queryperf/index.html', icon: 'book' },
-        { text: 'Notes on Chatty for LLMss Chrome Extension', href: '../../llms/chatty/help.html', icon: 'book' },
+        { text: 'Notes on Chatty for LLMs Chrome Extension', href: '../../llms/chatty/help.html', icon: 'book' },
         { text: 'Notes on Operating Systems', href: '../../pages/p.html?c=https://mohan-chinnappan-n5.github.io/pages/os/content.js', icon: 'book' },
 
       { text: 'All About Linux', href: '../../books/linux/index.html', icon: 'book' },
