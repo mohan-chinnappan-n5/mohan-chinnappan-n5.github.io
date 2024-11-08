33fec50da6446d5e3860c117209fa2e90bb2b300
mohan chinnappan <mchinnappan@apple.com>
2024-11-08 05:17:36 -0500

    glossary app added

--- apps/3/sections.js
+++ apps/3/sections.js
@@ -447,6 +447,16 @@ Chemistry: {
 
     },
 
+SFAI: {
+      title: 'agentforce',
+      links: [
+        { text: 'Glossary', href: '../../terms/terms.html?data=eai.json', icon: 'desktop' },
+ 
+        // Add more links here
+      ],
+
+    },
+
 
 
     // Add more sections here
