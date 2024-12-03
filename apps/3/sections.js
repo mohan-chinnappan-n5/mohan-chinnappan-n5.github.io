f44558ba348f5c519698c51e55b3a9b1b210a2c7
mohan chinnappan <mohan.chinnappan.n@gmail.com>
2024-12-03 07:38:06 -0500

    calc added

--- apps/3/sections.js
+++ apps/3/sections.js
@@ -144,12 +144,14 @@
         { text: 'Clock', href: '../..//utils/clock/clockapp.html', icon: 'desktop' },
         { text: 'Calendar', href: '../..//utils/cal/cal.html', icon: 'desktop' },
   
+        { text: 'Scientific Calculator', href: '../../calc/c.html', icon: 'desktop' },
 
 	{ text: 'Thank you! Card Generator', href: '../../cards/ty/ty.html', icon: 'desktop' },
         { text: 'Trivia', href: '../../trivia/trivia.html', icon: 'desktop' },
         { text: 'Vanity License Plate Generator', href: '../../common/license-plate/index.html', icon: 'desktop' },
         { text: 'US - Tax Calculator', href: 'https://mctaxes.streamlit.app', icon: 'desktop' },
         { text: 'Iframes viewer', href: '../../frames/iframes.html?data=frames.json', icon: 'desktop' },
+
 
 
 
